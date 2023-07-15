8ebe30685ff734d3967a9d671a96d64a5514683a
minihazel <decyanidez@gmail.com>
2023-07-15 16:28:35 +0200

    Add project files.

---
+++ mainForm.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace SPTManager
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.clientMods = new System.Windows.Forms.Panel();
+            this.serverModsBtn = new System.Windows.Forms.Button();
+            this.clientModsBtn = new System.Windows.Forms.Button();
+            this.serverInfoPanel = new System.Windows.Forms.Panel();
+            this.serverMods = new System.Windows.Forms.Panel();
+            this.globalFontSizeTitle = new System.Windows.Forms.Label();
+            this.globalFontSizeBox = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.globalFontSizeBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // clientMods
+            // 
+            this.clientMods.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.clientMods.AutoScroll = true;
+            this.clientMods.Location = new System.Drawing.Point(0, 0);
+            this.clientMods.Name = "clientMods";
+            this.clientMods.Size = new System.Drawing.Size(385, 468);
+            this.clientMods.TabIndex = 0;
+            // 
+            // serverModsBtn
+            // 
+            this.serverModsBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.serverModsBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.serverModsBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.serverModsBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(120)))), ((int)(((byte)(120)))));
+            this.serverModsBtn.FlatAppearance.BorderSize = 0;
+            this.serverModsBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.serverModsBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.serverModsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.serverModsBtn.Location = new System.Drawing.Point(1, 471);
+            this.serverModsBtn.Name = "serverModsBtn";
+            this.serverModsBtn.Size = new System.Drawing.Size(162, 36);
+            this.serverModsBtn.TabIndex = 1;
+            this.serverModsBtn.Text = "Server mods";
+            this.serverModsBtn.UseVisualStyleBackColor = false;
+            this.serverModsBtn.Click += new System.EventHandler(this.serverModsBtn_Click);
+            // 
+            // clientModsBtn
+            // 
+            this.clientModsBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.clientModsBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.clientModsBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(120)))), ((int)(((byte)(120)))));
+            this.clientModsBtn.FlatAppearance.BorderSize = 0;
+            this.clientModsBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.clientModsBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.clientModsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clientModsBtn.Location = new System.Drawing.Point(164, 471);
+            this.clientModsBtn.Name = "clientModsBtn";
+            this.clientModsBtn.Size = new System.Drawing.Size(162, 36);
+            this.clientModsBtn.TabIndex = 2;
+            this.clientModsBtn.Text = "Client mods";
+            this.clientModsBtn.UseVisualStyleBackColor = true;
+            this.clientModsBtn.Click += new System.EventHandler(this.clientModsBtn_Click);
+            // 
+            // serverInfoPanel
+            // 
+            this.serverInfoPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.serverInfoPanel.Location = new System.Drawing.Point(391, 0);
+            this.serverInfoPanel.Name = "serverInfoPanel";
+            this.serverInfoPanel.Size = new System.Drawing.Size(524, 468);
+            this.serverInfoPanel.TabIndex = 3;
+            // 
+            // serverMods
+            // 
+            this.serverMods.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.serverMods.AutoScroll = true;
+            this.serverMods.Location = new System.Drawing.Point(0, 0);
+            this.serverMods.Name = "serverMods";
+            this.serverMods.Size = new System.Drawing.Size(385, 468);
+            this.serverMods.TabIndex = 4;
+            // 
+            // globalFontSizeTitle
+            // 
+            this.globalFontSizeTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.globalFontSizeTitle.AutoSize = true;
+            this.globalFontSizeTitle.Location = new System.Drawing.Point(329, 477);
+            this.globalFontSizeTitle.Name = "globalFontSizeTitle";
+            this.globalFontSizeTitle.Size = new System.Drawing.Size(110, 17);
+            this.globalFontSizeTitle.TabIndex = 5;
+            this.globalFontSizeTitle.Text = "Global font size:";
+            // 
+            // globalFontSizeBox
+            // 
+            this.globalFontSizeBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.globalFontSizeBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.globalFontSizeBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.globalFontSizeBox.ForeColor = System.Drawing.Color.LightGray;
+            this.globalFontSizeBox.Location = new System.Drawing.Point(445, 478);
+            this.globalFontSizeBox.Name = "globalFontSizeBox";
+            this.globalFontSizeBox.Size = new System.Drawing.Size(56, 20);
+            this.globalFontSizeBox.TabIndex = 6;
+            this.globalFontSizeBox.ValueChanged += new System.EventHandler(this.globalFontSizeBox_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(120)))), ((int)(((byte)(120)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(753, 471);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 36);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Add new mod";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.ClientSize = new System.Drawing.Size(927, 508);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.globalFontSizeBox);
+            this.Controls.Add(this.globalFontSizeTitle);
+            this.Controls.Add(this.serverInfoPanel);
+            this.Controls.Add(this.clientModsBtn);
+            this.Controls.Add(this.serverModsBtn);
+            this.Controls.Add(this.serverMods);
+            this.Controls.Add(this.clientMods);
+            this.Font = new System.Drawing.Font("Bahnschrift Light", 10F);
+            this.ForeColor = System.Drawing.Color.LightGray;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SPT Manager";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.mainForm_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.globalFontSizeBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel clientMods;
+        private System.Windows.Forms.Button serverModsBtn;
+        private System.Windows.Forms.Button clientModsBtn;
+        private System.Windows.Forms.Panel serverInfoPanel;
+        private System.Windows.Forms.Panel serverMods;
+        private System.Windows.Forms.Label globalFontSizeTitle;
+        private System.Windows.Forms.NumericUpDown globalFontSizeBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ mainForm.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SPTManager
+{
+    public partial class mainForm : Form
+    {
+        public string currentDir = "E:\\SPT Iterations\\SPT-AKI 3.5.8";
+
+        public string userFolder;
+        public string serverModsFolder;
+        public string clientModsFolder;
+
+        public Color listBackcolor = Color.FromArgb(255, 28, 28, 28);
+        public Color listSelectedcolor = Color.FromArgb(255, 37, 37, 37);
+        public Color listHovercolor = Color.FromArgb(255, 33, 33, 33);
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            serverMods.BringToFront();
+
+            // Variables
+            userFolder = Path.Combine(currentDir, "user");
+            serverModsFolder = Path.Combine(userFolder, "mods");
+            string clientBepFolder = Path.Combine(currentDir, "BepInEx");
+            clientModsFolder = Path.Combine(clientBepFolder, "plugins");
+
+            // Checking paths
+            bool userFolderExists = Directory.Exists(userFolder);
+            bool serverModsFolderExists = Directory.Exists(serverModsFolder);
+            bool clientModsFolderExists = Directory.Exists(clientModsFolder);
+
+            if (userFolderExists && serverModsFolderExists && clientModsFolderExists)
+            {
+                serverModsBtn.BackColor = Color.FromArgb(37, 37, 37);
+
+                listServerMods();
+                listClientMods();
+
+                globalFontSizeBox.Value = 10;
+            }
+        }
+
+        private void globalFontSizeBox_ValueChanged(object sender, EventArgs e)
+        {
+            foreach (Control ctrl in serverMods.Controls)
+            {
+                if (ctrl is Label lbl)
+                {
+                    lbl.Font = new Font("Bahnschrift Light", Convert.ToSingle(globalFontSizeBox.Value), FontStyle.Regular);
+                }
+            }
+
+            foreach (Control ctrl in serverMods.Controls)
+            {
+                if (ctrl is Label lbl)
+                {
+                    lbl.Font = new Font("Bahnschrift Light", Convert.ToSingle(globalFontSizeBox.Value), FontStyle.Regular);
+                }
+            }
+        }
+
+        private void listServerMods()
+        {
+            string[] serverModsPath = Directory.GetDirectories(serverModsFolder);
+            int widthspacer = 38;
+
+            for (int i = 0; i < serverModsPath.Length; i++)
+            {
+                Label lbl = new Label();
+                lbl.Text = Path.GetFileName(serverModsPath[i]);
+                lbl.AutoSize = false;
+                lbl.Anchor = (AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right);
+                lbl.TextAlign = ContentAlignment.MiddleLeft;
+                lbl.Size = new Size(serverMods.Size.Width - 4, widthspacer);
+                lbl.Location = new Point(0, 1 + (i * widthspacer));
+                lbl.Font = new Font("Bahnschrift Light", 10, FontStyle.Regular);
+                lbl.BackColor = listBackcolor;
+                lbl.ForeColor = Color.LightGray;
+                lbl.Margin = new Padding(1, 1, 1, 1);
+                lbl.Cursor = Cursors.Hand;
+                lbl.MouseEnter += new EventHandler(lbl_MouseEnter);
+                lbl.MouseLeave += new EventHandler(lbl_MouseLeave);
+                lbl.MouseDown += new MouseEventHandler(lbl_MouseDown);
+                lbl.MouseUp += new MouseEventHandler(lbl_MouseUp);
+                serverMods.Controls.Add(lbl);
+            }
+        }
+
+        private void listClientMods()
+        {
+            string[] clientModsPath = Directory.GetDirectories(clientModsFolder);
+            string[] clientModsPath2 = Directory.GetFiles(clientModsFolder);
+            int widthspacer1 = 38;
+            int widthspacer2 = 38;
+            int labelIndex = 0;
+            int labelIndex2 = 0;
+
+            for (int i = 0; i < clientModsPath.Length; i++)
+            {
+                Label lbl = new Label();
+                lbl.Text = Path.GetFileName(clientModsPath[i]);
+                lbl.AutoSize = false;
+                lbl.Anchor = (AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right);
+                lbl.TextAlign = ContentAlignment.MiddleLeft;
+                lbl.Size = new Size(serverMods.Size.Width - 4, widthspacer1);
+                lbl.Location = new Point(0, 1 + (labelIndex * widthspacer1));
+                lbl.Font = new Font("Bahnschrift Light", 10, FontStyle.Regular);
+                lbl.BackColor = listBackcolor;
+                lbl.ForeColor = Color.LightGray;
+                lbl.Margin = new Padding(1, 1, 1, 1);
+                lbl.Cursor = Cursors.Hand;
+                lbl.MouseEnter += new EventHandler(lbl_MouseEnter);
+                lbl.MouseLeave += new EventHandler(lbl_MouseLeave);
+                lbl.MouseDown += new MouseEventHandler(lbl_MouseDown);
+                lbl.MouseUp += new MouseEventHandler(lbl_MouseUp);
+                clientMods.Controls.Add(lbl);
+                labelIndex++;
+            }
+
+            for (int i = 0; i < clientModsPath2.Length; i++)
+            {
+                if (Path.GetFileNameWithoutExtension(clientModsPath2[i]) != "aki-core" &&
+                    Path.GetFileNameWithoutExtension(clientModsPath2[i]) != "aki-custom" &&
+                    Path.GetFileNameWithoutExtension(clientModsPath2[i]) != "aki-debugging" &&
+                    Path.GetFileNameWithoutExtension(clientModsPath2[i]) != "aki-singleplayer" &&
+                    Path.GetFileNameWithoutExtension(clientModsPath2[i]) != "ConfigurationManager" &&
+                    Path.GetFileNameWithoutExtension(clientModsPath2[i]) != "LICENSE" &&
+                    Path.GetExtension(clientModsPath2[i]) != null || Path.GetExtension(clientModsPath2[i]) != "")
+                {
+                    Label lbl = new Label();
+                    lbl.Text = Path.GetFileName(clientModsPath2[i]);
+                    lbl.AutoSize = false;
+                    lbl.Anchor = (AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right);
+                    lbl.TextAlign = ContentAlignment.MiddleLeft;
+                    lbl.Size = new Size(serverMods.Size.Width - 4, widthspacer2);
+                    lbl.Location = new Point(0, 1 + (labelIndex2 * widthspacer2));
+                    lbl.Font = new Font("Bahnschrift Light", 10, FontStyle.Regular);
+                    lbl.BackColor = listBackcolor;
+                    lbl.ForeColor = Color.LightGray;
+                    lbl.Margin = new Padding(1, 1, 1, 1);
+                    lbl.Cursor = Cursors.Hand;
+                    lbl.MouseEnter += new EventHandler(lbl_MouseEnter);
+                    lbl.MouseLeave += new EventHandler(lbl_MouseLeave);
+                    lbl.MouseDown += new MouseEventHandler(lbl_MouseDown);
+                    lbl.MouseUp += new MouseEventHandler(lbl_MouseUp);
+                    clientMods.Controls.Add(lbl);
+                    labelIndex2++;
+                }
+            }
+        }
+
+        private void lbl_MouseEnter(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label label = (System.Windows.Forms.Label)sender;
+            if (label.Text != "")
+            {
+                if (label.BackColor != listSelectedcolor)
+                {
+                    label.BackColor = listHovercolor;
+                }
+            }
+        }
+
+        private void lbl_MouseLeave(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label label = (System.Windows.Forms.Label)sender;
+            if (label.Text != "")
+            {
+                if (label.BackColor != listSelectedcolor)
+                {
+                    label.BackColor = listBackcolor;
+                }
+            }
+        }
+
+        // When a profile is clicked
+        private void lbl_MouseDown(object sender, MouseEventArgs e)
+        {
+            System.Windows.Forms.Label label = (System.Windows.Forms.Label)sender;
+
+            if (label.Text != "")
+            {
+                if (Control.ModifierKeys == Keys.Control && e.Button == MouseButtons.Left)
+                {
+                    label.BackColor = listSelectedcolor;
+                }
+                else if ((Control.ModifierKeys & Keys.Control) != Keys.Control)
+                {
+                    deSelectPanel(label.Parent);
+                    label.BackColor = listSelectedcolor;
+                }
+            }
+        }
+
+        private void lbl_MouseUp(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label label = (System.Windows.Forms.Label)sender;
+            if (label.Text != "")
+            {
+                // label.BackColor = listHovercolor;
+            }
+        }
+
+        private void deSelectPanel(Control parent)
+        {
+            foreach (Control ctrl in parent.Controls)
+            {
+                if (ctrl is Label lbl)
+                {
+                    lbl.BackColor = listBackcolor;
+                }
+            }
+        }
+
+        private void serverModsBtn_Click(object sender, EventArgs e)
+        {
+            serverMods.BringToFront();
+            serverModsBtn.BackColor = Color.FromArgb(37, 37, 37);
+            clientModsBtn.BackColor = Color.FromArgb(28, 28, 28);
+        }
+
+        private void clientModsBtn_Click(object sender, EventArgs e)
+        {
+            clientMods.BringToFront();
+            clientModsBtn.BackColor = Color.FromArgb(37, 37, 37);
+            serverModsBtn.BackColor = Color.FromArgb(28, 28, 28);
+        }
+
+        private void mainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                if (serverModsBtn.BackColor == listSelectedcolor)
+                {
+                    
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
